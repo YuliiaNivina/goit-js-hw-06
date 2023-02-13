83d53f1ef25085663a2e55275b7be7df0bea070a
Yuliia Nivina <y.nivina@gmail.com>
2023-02-13 23:33:53 +0200

    Add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.currentTarget.elements.email.value;
+    const pass = event.currentTarget.elements.password.value;
+
+    if (mail === "" || pass === "") {
+        return alert("All fields must be filled!");
+    }
+    
+    const formFilling = {mail, pass};
+    console.log(formFilling);
+
+    event.currentTarget.reset();
+}
