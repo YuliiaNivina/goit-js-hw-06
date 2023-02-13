fc846a1f6b9f82f8f00b4ae1352140ab4cee4f0c
Yuliia Nivina <y.nivina@gmail.com>
2023-02-14 00:01:53 +0200

    Add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", changeBodyColor);
+
+function changeBodyColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = getRandomHexColor();
+}
